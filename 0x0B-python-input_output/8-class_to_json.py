2f242048e7e99a267a733192a5b3720dc9356f09
Isaiah-Talabi <isaiahtalabi9@gmail.com>
2023-01-10 19:48:29 +0100

    ijneraa

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a Python class-to-JSON function."""
+
+
+def class_to_json(obj):
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__https://github.com/NwabuezeFranklin/alx-higher_level_programming/blob/master/0x0B-python-input_output/9-student.py
