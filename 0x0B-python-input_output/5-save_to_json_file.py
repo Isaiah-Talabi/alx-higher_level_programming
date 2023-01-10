7a63ff93c986552bf146fe2bde25b118743826fc
Isaiah-Talabi <isaiahtalabi9@gmail.com>
2023-01-10 20:05:11 +0100

    ijneraa

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """Write an object to a text file using JSON representation."""
     with open(filename, "w") as f:
-        json.dump(my_obj, f)https://github.com/NwabuezeFranklin/alx-higher_level_programming/blob/master/0x0B-python-input_output/6-load_from_json_file.py
+        json.dump(my_obj, f)
--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,4 +4,4 @@
 
 def class_to_json(obj):
     """Return the dictionary represntation of a simple data structure."""
-    return obj.__dict__https://github.com/NwabuezeFranklin/alx-higher_level_programming/blob/master/0x0B-python-input_output/9-student.py
+    return obj.__dict__
